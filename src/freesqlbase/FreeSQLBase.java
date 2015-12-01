b6cb97d97e45cb27b65a2a034b73c5078d84b43b
rafaliang <617618803@qq.com>
2015-12-01 13:06:17 +0800

    Day2 task2 3

--- src/freesqlbase/FreeSQLBase.java
+++ src/freesqlbase/FreeSQLBase.java
@@ -2,6 +2,9 @@ package freesqlbase;
 
 import java.io.*;
 import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.zip.GZIPInputStream;
 
 
@@ -10,6 +13,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class FreeSQLBase {
@@ -22,6 +26,9 @@ public class FreeSQLBase {
 	static String propertysql = "";
 	static String image_imgsrcsql = "";
 	static String imgsrc_urisql = "";
+	static String entity_typesql="";
+	static String property_schemasql="";
+	static String property_expsql="";
 	static int id = 0;
 	static String obj1 = null;
 	static String prop = null;
@@ -36,6 +43,7 @@ public class FreeSQLBase {
 	
 	
 	static Thread readth = new Thread() {
+		@SuppressWarnings("null")
 		@Override
 		public void run() {
 			String line;
@@ -43,28 +51,71 @@ public class FreeSQLBase {
 			String mid = null;
 			String name = null;
 			String description = null;
-			String idstring = null;
+			
 			String image=null;
 			String imgsrc=null;
 			String uri=null;
-			int type = 0;
+			int type = -1;
 			int rank = 0;
-			int cnt0=1,cnt1=1,cnt2=1,cnt3=1,cnt4=1,cnt5=1;
+			int cnt1=1,cnt2=1,cnt3=1;
+			Boolean is_ei_read = false, is_pi_read=false;;
+			Integer entity_id = 0, property_id = 0, type_id=0;
 			String obj1_prev=null;
-			
-			/*PreparedStatement pstmt1 = null,pstmt2 = null,pstmt3=null,pstmt4=null,pstmt5=null,pstmt0=null;
-			try
-			{
-				pstmt1=con.prepareStatement("INSERT INTO domain VALUES (?,?,?,?)");
-				pstmt2=con.prepareStatement("INSERT INTO type VALUES (?,?,?,?)");
-				pstmt3=con.prepareStatement("INSERT INTO property VALUES (?,?,?,?)");
-				pstmt4=con.prepareStatement("INSERT INTO image_imgsrc VALUES (?,?)");
-				pstmt5=con.prepareStatement("INSERT INTO imgsrc_uri VALUES (?,?)");
-				pstmt0=con.prepareStatement("INSERT INTO entity VALUES (?,?,?,?,?,?)");
+			String entity=null, property = null;
+			//String type_mid;
+			String type_idstring = null;
+			HashMap<String,Integer> tidstring_id=new HashMap<String, Integer>();
+			HashMap<String,Integer> pmid_id=new HashMap<String, Integer>();
+			Iterator iter=null;
+			PreparedStatement pstmt_ei = null, pstmt_ti = null, pstmt_pi = null;
+			ResultSet rs_ei = null, rs_ti = null, rs_pi = null;
+			try {
+				//pstmt_ei=con.prepareStatement("select mid,id from entity",ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+				//pstmt_ei.setFetchSize(Integer.MIN_VALUE);  
+				//pstmt_ei.setFetchDirection(ResultSet.FETCH_REVERSE);  
+				//pstmt_pi=con.prepareStatement("select mid,id from property");
+				pstmt_ti=con.prepareStatement("select idstring,id from type");
+				//rs_ei=pstmt_ei.executeQuery();
+				//rs_pi=pstmt_pi.executeQuery();
+				rs_ti=pstmt_ti.executeQuery();
+				while (rs_ti.next())
+				{
+					tidstring_id.put(rs_ti.getString("idstring"), rs_ti.getInt("id"));
+				}
+				rs_ti.close();
+				pstmt_ti.close();
+				
+				pstmt_pi=con.prepareStatement("select mid,id from property");
+				rs_pi=pstmt_pi.executeQuery();
+				while (rs_pi.next())
+				{
+					pmid_id.put(rs_pi.getString("mid"), rs_pi.getInt("id"));
+				}
+				rs_pi.close();
+				pstmt_pi.close();
+				//iter=pmid_id.entrySet().iterator();
+				
+				/*pstmt_ei=con.prepareStatement("select mid,id from entity",ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+				pstmt_ei.setFetchSize(Integer.MIN_VALUE);  
+				pstmt_ei.setFetchDirection(ResultSet.FETCH_REVERSE);
+				rs_ei=pstmt_ei.executeQuery();*/
+				/*if (rs_ei.next())
+				{
+					entity=rs_ei.getString("mid");
+					entity_id=rs_ei.getInt("id");
+				}
+				else
+					is_ei_read=true;
+				if (rs_pi.next())
+				{
+					property=rs_pi.getString("mid");
+					property_id=rs_pi.getInt("id");
+				}
+				else
+					is_pi_read=true;*/
+			} catch (SQLException e1) {
+				e1.printStackTrace();
 			}
-			catch (Exception e) {
-				System.out.println("MYSQL ERROR:" + e.getMessage());
-			}*/
 			
 			while (true)
 			{
@@ -75,41 +126,19 @@ public class FreeSQLBase {
 						try {
 							Statement stmt;
 							stmt = con.createStatement();
-							if (!domainsql.equals(""))
+							if (!entity_typesql.equals(""))
 							{
-								//pstmt1.executeBatch();
-								stmt.executeUpdate("INSERT INTO domain VALUES "+domainsql.substring(0,domainsql.length()-1)+";");
-								
+								stmt.executeUpdate("INSERT INTO entity_type VALUES "+entity_typesql.substring(0,entity_typesql.length()-1)+";");
 							}
 								
-							if (!typesql.equals(""))
+							if (!property_schemasql.equals(""))
 							{
-								//pstmt2.executeBatch();
-								stmt.executeUpdate("INSERT INTO type VALUES "+typesql.substring(0,typesql.length()-1)+";");
+								stmt.executeUpdate("INSERT INTO property_schema VALUES "+property_schemasql.substring(0,property_schemasql.length()-1)+";");
 							}
 								
-							if (!propertysql.equals(""))
+							if (!property_expsql.equals(""))
 							{
-								//System.out.println("INSERT INTO property VALUES "+propertysql.substring(0,propertysql.length()-1)+";");
-								stmt.executeUpdate("INSERT INTO property VALUES "+propertysql.substring(0,propertysql.length()-1)+";");
-							}
-								
-							if (!entitysql.equals(""))
-							{
-								//System.out.println("INSERT INTO entity VALUES "+entitysql.substring(0,entitysql.length()-1)+";");
-								stmt.executeUpdate("INSERT INTO entity VALUES "+entitysql.substring(0,entitysql.length()-1)+";");
-							}
-								
-							if (!image_imgsrcsql.equals(""))
-							{
-								//System.out.println("INSERT INTO image_imgsrc VALUES "+image_imgsrcsql.substring(0,image_imgsrcsql.length()-1)+";");
-								stmt.executeUpdate("INSERT INTO image_imgsrc VALUES "+image_imgsrcsql.substring(0,image_imgsrcsql.length()-1)+";");
-							}
-								
-							if (!imgsrc_urisql.equals(""))
-							{
-								//System.out.println("INSERT INTO imgsrc_uri VALUES "+imgsrc_urisql.substring(0,imgsrc_urisql.length()-1)+";");
-								stmt.executeUpdate("INSERT INTO imgsrc_uri VALUES "+imgsrc_urisql.substring(0,imgsrc_urisql.length()-1)+";");
+								stmt.executeUpdate("INSERT INTO property_expectedtype VALUES "+property_expsql.substring(0,property_expsql.length()-1)+";");
 							}
 							stmt.close();	
 						} catch (Exception e) {
@@ -117,6 +146,50 @@ public class FreeSQLBase {
 						}
 						break;
 					}
+					
+					if (cnt1%10001==0)
+					{
+						System.out.println(entity_id/88767079.0f);
+						try {
+							Statement stmt;
+							stmt = con.createStatement();
+							stmt.executeUpdate("INSERT INTO entity_type VALUES "+entity_typesql.substring(0,entity_typesql.length()-1)+";");
+							stmt.close();
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}
+						cnt1=1;
+						entity_typesql="";
+					}
+					
+					if (cnt2%10001==0)
+					{
+						try {
+							Statement stmt;
+							stmt = con.createStatement();
+							stmt.executeUpdate("INSERT INTO property_schema VALUES "+property_schemasql.substring(0,property_schemasql.length()-1)+";");
+							stmt.close();
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}
+						cnt2=1;
+						property_schemasql="";
+					}
+					
+					if (cnt3%10001==0)
+					{
+						try {
+							Statement stmt;
+							stmt = con.createStatement();
+							stmt.executeUpdate("INSERT INTO property_expectedtype VALUES "+property_expsql.substring(0,property_expsql.length()-1)+";");
+							stmt.close();
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}
+						cnt3=1;
+						property_expsql="";
+					}
+					
 					
 					String[] sp =line.split("\t");
 					obj1 = sp[0].substring(1, sp[0].length()-1);
@@ -132,246 +205,70 @@ public class FreeSQLBase {
 					continue;
 				if (!obj1.equals(obj1_prev))
 				{
-					if (obj1_prev!=null)
-					{
-						if (type==1)
-						{
-							domainsql+=String.format("(%d,%s,%s,%s),", id, mid, name, idstring);
-							cnt1++;
-						}
-						else if (type==2)
-						{
-							//id++;
-							//System.out.printf("%s,%s,%s,%d,%d\n",mid, name, idstring, rank, id);
-							typesql+=String.format("(%d,%s,%s,%s),", id, mid, name, idstring);
-							cnt2++;
-						}
-						else if (type==3)
-						{
-							//id++;
-							//System.out.printf("%s,%s,%s,%d,%d\n",mid, name, idstring, rank, id);
-							propertysql+=String.format("(%d,%s,%s,%s),", id, mid, name, idstring);
-							cnt3++;
-						}
-						else if (type==4)
-						{
-							//id++;
-							//System.out.printf("%s,%s\n",mid, imgsrc);
-							image_imgsrcsql+=String.format("(%s,%s),", mid, imgsrc);
-							cnt4++;
-						}
-						else if (type==5)
-						{
-							//id++;
-							//System.out.printf("%s,%s\n",mid, uri);
-							imgsrc_urisql+=String.format("(%s,%s),", mid, uri);
-							cnt5++;
-						}
-						else
-						{
-							/*id++;
-							if (image!=null)
-								System.out.printf("%s,%s,%d,%d,%s,%s\n",mid, name, rank, id, description, image);*/
-							entitysql+=String.format("(%d,%s,%d,%s,%s,%s),", id, mid, rank, name, description, image);
-							cnt0++;
-						}
-						
-						if (cnt0%1000==0)
-						{
-							System.out.println(id/88788059.0f);
-							try {
-								Statement stmt;
-								stmt = con.createStatement();
-								stmt.executeUpdate("INSERT INTO entity VALUES "+entitysql.substring(0,entitysql.length()-1)+";");
-								stmt.close();	
-							} catch (Exception e) {
-								System.out.println("MYSQL ERROR:" + e.getMessage());
-							}
-							cnt0=1;
-							entitysql = "";
-						}
-						
-						if (cnt1%1000==0)
-						{
-							System.out.println(id/88788059.0f);
-							try {
-								Statement stmt;
-								stmt = con.createStatement();
-								stmt.executeUpdate("INSERT INTO domain VALUES "+domainsql.substring(0,domainsql.length()-1)+";");
-								stmt.close();	
-							} catch (Exception e) {
-								System.out.println("MYSQL ERROR:" + e.getMessage());
-							}
-							cnt1=1;
-							domainsql = "";
-						}
-						
-						if (cnt2%1000==0)
-						{
-							System.out.println(id/88788059.0f);
-							try {
-								Statement stmt;
-								stmt = con.createStatement();
-								stmt.executeUpdate("INSERT INTO type VALUES "+typesql.substring(0,typesql.length()-1)+";");
-								stmt.close();	
-							} catch (Exception e) {
-								System.out.println("MYSQL ERROR:" + e.getMessage());
-							}
-							cnt2=1;
-							typesql = "";
-						}
-						
-						if (cnt3%1000==0)
-						{
-							System.out.println(id/88788059.0f);
-							try {
-								Statement stmt;
-								stmt = con.createStatement();
-								stmt.executeUpdate("INSERT INTO property VALUES "+propertysql.substring(0,propertysql.length()-1)+";");
-								stmt.close();	
-							} catch (Exception e) {
-								System.out.println("MYSQL ERROR:" + e.getMessage());
-							}
-							cnt3=1;
-							propertysql = "";
-						}
-						
-						if (cnt4%1000==0)
-						{
-							System.out.println(id/88788059.0f);
-							try {
-								Statement stmt;
-								stmt = con.createStatement();
-								stmt.executeUpdate("INSERT INTO image_imgsrc VALUES "+image_imgsrcsql.substring(0,image_imgsrcsql.length()-1)+";");
-								stmt.close();	
-							} catch (Exception e) {
-								System.out.println("MYSQL ERROR:" + e.getMessage());
-							}
-							cnt4=1;
-							image_imgsrcsql = "";
-						}
-						
-						if (cnt5%1000==0)
-						{
-							System.out.println(id/88788059.0f);
-							try {
-								Statement stmt;
-								stmt = con.createStatement();
-								stmt.executeUpdate("INSERT INTO imgsrc_uri VALUES "+imgsrc_urisql.substring(0,imgsrc_urisql.length()-1)+";");
-								stmt.close();	
-							} catch (Exception e) {
-								System.out.println("MYSQL ERROR:" + e.getMessage());
-							}
-							cnt5=1;
-							imgsrc_urisql = "";
-						}
-						
-						
-						
-						
-					}
 					obj1_prev=obj1;
-					id++;
-					rank=0;
-					mid = null;
-					name = null;
-					description = null;
-					idstring = null;
-					image=null;
 					type = 0;
+					entity_id = null;
+					property_id = null;
+					type_id = null;
 					mid=nspart[1];
 					mid=mid.substring(2,mid.length());
 					if (mid.length()>98)
 						mid=mid.substring(0,98);
-					mid="\""+mid+"\"";
+					try {
+						pstmt_ei=con.prepareStatement("select id from entity where mid=?");
+						pstmt_ei.setString(1, mid);
+						rs_ei=pstmt_ei.executeQuery();
+						if (rs_ei.next())
+							entity_id=rs_ei.getInt("id");
+						rs_ei.close();
+						pstmt_ei.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					property_id=pmid_id.get(mid);
+					if (entity_id!=null)
+					{
+						type=1;
+					}
+					else if (property_id!=null)
+						type=2;
+					else type=0;
 				}
-				rank++;
-				if (prop.indexOf("type.object.id")!=-1)
+				if (type==0)
+					continue;
+				else if (type==1)
 				{
-					String[] obj2_new = obj2.substring(1, obj2.length()-1).split("ns/");
-					idstring = obj2_new[obj2_new.length-1];
-					idstring=idstring.substring(1,idstring.length());
-					idstring=idstring.replaceAll("/",".");
-					if (idstring.length()>98)
-						idstring=idstring.substring(0,98);
-					idstring="\""+idstring+"\"";
-				}
-				else if (prop.indexOf("type.object.type")!=-1)
-				{
-					if (obj2.indexOf("type.domain")!=-1)
+					if (prop.indexOf("type.object.type")!=-1)
 					{
-						type = 1;
-					}
-					if (obj2.indexOf("type.type")!=-1)
-					{
-						type = 2;
-					}
-					if (obj2.indexOf("type.property")!=-1)
-					{
-						type = 3;
-					}
-					if (obj2.indexOf("common.image")!=-1)
-					{
-						type = 4;
-					}
-					if (obj2.indexOf("type.content_import")!=-1)
-					{
-						type = 5;
-					}
-				}
-				else if (prop.indexOf("type.object.name")!=-1)
-				{
-					if (obj2.endsWith("@en"))
-					{
-						name=obj2.substring(1, obj2.length()-4);
-						if (name!=null)
-							name=name.replaceAll("\"", "'");
-						if (name.length()>29998)
-							name=name.substring(0,29998);
-						name = "\""+name+"\"";
+						String[] nspart2 = obj2.split("ns/");
+						type_idstring=nspart2[1];
+						type_idstring=type_idstring.substring(0,type_idstring.length()-1);
+						type_id=tidstring_id.get(type_idstring);
+						//System.out.println(type_idstring+" "+type_id);
+						entity_typesql+=String.format("(%d,%d),", entity_id, type_id);	
+						cnt1++;
 					}
 				}
-				else if (prop.indexOf("common.topic.description")!=-1)
+				else if (type==2)
 				{
-					if (obj2.endsWith("@en"))
+					if (prop.indexOf("type.property.schema")!=-1)
 					{
-						
-						description = obj2.substring(1, obj2.length()-4);
-						if (description!=null)
-						{
-							description=description.replaceAll("\"", "'");
-							description=description.replaceAll("\\n"," ");
-							//description=description.replaceAll("\\"," ");
-						}
-							
-						if (description.length()>1998)
-							description="\""+description.substring(0,1998)+"\"";
-						else
-							description="\""+description+"\"";
+						String[] nspart2 = obj2.split("ns/");
+						type_idstring=nspart2[1];
+						type_idstring=type_idstring.substring(0,type_idstring.length()-1);
+						type_id=tidstring_id.get(type_idstring);
+						property_schemasql+=String.format("(%d,%d),", property_id, type_id);	
+						cnt2++;
 					}
-				}
-				else if (prop.indexOf("common.topic.image")!=-1)
-				{
-					String[] obj2_new = obj2.substring(1, obj2.length()-1).split("ns/m.");
-					image=obj2_new[obj2_new.length-1];
-					if (image.length()>98)
-						image=image.substring(0,98);
-					image = "\""+image+"\"";
-				}
-				else if (prop.indexOf("type.content.source")!=-1)
-				{
-					String[] obj2_new = obj2.substring(1, obj2.length()-1).split("ns/m.");
-					imgsrc=obj2_new[obj2_new.length-1];
-					if (imgsrc.length()>98)
-						imgsrc=imgsrc.substring(0,98);
-					imgsrc = "\""+imgsrc+"\"";
-				}
-				else if (prop.indexOf("type.content_import.uri")!=-1)
-				{
-					uri=obj2.substring(1,obj2.length()-1);
-					if (uri.length()>498)
-						uri=uri.substring(0,498);
-					uri = "\""+uri+"\"";
+					else if (prop.indexOf("type.property.expected_type")!=-1)
+					{
+						String[] nspart2 = obj2.split("ns/");
+						type_idstring=nspart2[1];
+						type_idstring=type_idstring.substring(0,type_idstring.length()-1);
+						type_id=tidstring_id.get(type_idstring);
+						property_expsql+=String.format("(%d,%d),", property_id, type_id);	
+						cnt3++;
+					}
 				}
 			}
 		}
@@ -383,14 +280,16 @@ public class FreeSQLBase {
 			try {
 				//Connection con = null; // 定义一个MYSQL链接对象
 				Class.forName("com.mysql.jdbc.Driver").newInstance(); // MYSQL驱动
-				con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Freebase", "root","thisismysql"); // 链接本地MYSQL				
+				//con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Freebase", "root","thisismysql"); 
+				con = DriverManager.getConnection("jdbc:mysql://202.120.37.25:23334/Freebase", "root","thisismysql"); // 链接本地MYSQL
 				} catch (Exception e) {
 					System.out.println("MYSQL ERROR:" + e.getMessage());
 				}
 
 			pos = new PipedOutputStream(); pis = new PipedInputStream(pos);
 			FileInputStream s = new FileInputStream(
-					new File("/home/freebase.gz")); 
+					//new File("/home/freebase.gz")); 
+					new File("H:/freebase.gz")); 
 			readth.start();
 			decompress(s);
 			 
